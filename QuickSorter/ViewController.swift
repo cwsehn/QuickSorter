495b205f7a9e811776f82a0475c953918c99dacf
cwsehn <cwsehn@gmail.com>
2018-02-28 09:07:30 -0600

    Initial Commit

---
+++ QuickSorter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QuickSorter
+//
+//  Created by Chris William Sehnert on 2/28/18.
+//  Copyright © 2018 InSehnDesigns. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
